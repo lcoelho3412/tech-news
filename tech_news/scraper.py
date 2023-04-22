af655d84ec8ae9cd7a36c27b767100b29a86e9d2
lcoelho3412 <leandrocoelho3@yahoo.com>
2023-04-22 11:35:35 -0300

    feat: :sparkles: Fetches and scrapes the specified amount of tech news, Scrapes the news details from the HTML content.

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,9 +1,9 @@
 import requests
-from time import sleep
 from parsel import Selector
+from tech_news.database import create_news
+from time import sleep
 
 
-# Requisito 1
 def fetch(url):
     sleep(1)
     try:
@@ -16,23 +16,53 @@ def fetch(url):
         return None
 
 
-# Requisito 2
 def scrape_updates(html_content):
     html_selector = Selector(text=html_content)
     update_links = html_selector.css("h2.entry-title a::attr(href)").getall()
     return update_links
 
 
-# Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_page_link = selector.css("a.next::attr(href)").get()
+    return next_page_link
 
 
-# Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    return {
+        "url": selector.css("link[rel='canonical']::attr(href)").get(),
+        "title": selector.css("h1.entry-title::text").get().strip(),
+        "timestamp": selector.css("li.meta-date::text").get(),
+        "writer": selector.css("span.author a::text").get(),
+        "reading_time": int(
+            selector.css("li.meta-reading-time::text").re_first(r"\d+")
+        ),
+        "summary": "".join(
+            selector.css(".entry-content > p:first-of-type *::text").getall()
+        ).strip(),
+        "category": selector.css("div.meta-category span.label::text").get(),
+    }
 
 
-# Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    next_page = "https://blog.betrybe.com"
+    news = []
+
+    while len(news) < amount:
+        content = fetch(next_page)
+        urls = scrape_updates(content)
+
+        for url in urls:
+            if len(news) < amount:
+                new_content = fetch(url)
+                piece = scrape_news(new_content)
+                news.append(piece)
+
+        next_page = scrape_next_page_link(content)
+        if not next_page:
+            break
+
+    create_news(news)
+    return news
