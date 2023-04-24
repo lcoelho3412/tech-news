7ce53cf2fbacf0ebc1717fbcd7f99aad9d7a38a3
lcoelho3412 <leandrocoelho3@yahoo.com>
2023-04-24 10:15:58 -0300

    feat: :sparkles: adds 3 new functions
    Search by title, by date and by category

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,13 +1,32 @@
+from tech_news.database import search_news
+from datetime import datetime
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({"title": {"$regex": title, "$options": 'i'}})
+    return [
+        (new["title"], new["url"])
+        for new in news
+    ]
 
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        formated = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        return [
+            (new["title"], new["url"])
+            for new in search_news({"timestamp": {"$eq": formated}})
+        ]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = search_news({"category": {"$regex": category, "$options": 'i'}})
+    return [
+        (new["title"], new["url"])
+        for new in news
+    ]
